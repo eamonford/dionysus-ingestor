f5f63e628f95b675bf9bd54de982f81edbfb314a
Eamon Ford <eamonford@gmail.com>
2016-12-10 01:25:08 -0600

    added everything to docker-compose

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM resin/rpi-raspbian
 RUN apt-get update && apt-get install -y python-pip && apt-get install sqlite3 && pip install paho-mqtt && pip install spyrk
 RUN mkdir /ingestor
 COPY . /ingestor
-CMD ["python", "/ingestor/main.py"]
+CMD ["python", "-u", "/ingestor/main.py"]
--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import OperationsScheduler
 from dao.SensorDataAccessor import SensorDataAccessor
 import sqlite3
 
-
 ACCESS_TOKEN = 'a520cd5bd34112b273fda91b1164f011b81fd2de'
 
 def on_connect(client, userdata, flags, rc):
@@ -15,26 +14,23 @@ def on_connect(client, userdata, flags, rc):
 def getReadingsAndPublish(args):
     client = args[0]
     spark = args[1]
-
     sensors = SensorDataAccessor().getAll()
     for sensor in sensors:
         try:
             value = spark.devices[sensor["name"]].read()
-            client.publish(
-                "dionysus/moisture",
-                "{device_id: '" + sensor["device_id"] + "', value: '" + str(value) + "'}")
+            jsonString = "{\"device_id\": \"" + sensor["device_id"] + "\", \"value\": " + str(value) + "}"
+            print("logging to dionysus/moisture: " + jsonString)
+            client.publish("dionysus/moisture", jsonString)
         except:
             logging.error("Error: unable to get/publish reading from sensor" + sensor["name"] + ". Sensor may be offline.")
 
 def main():
     sparkClient = SparkCloud(ACCESS_TOKEN)
-
     mqttClient = mqtt.Client()
     mqttClient.on_connect = on_connect
     mqttClient.connect("eamonford.hopto.org", 1883, 60)
 
-    OperationsScheduler.asyncRunAtInterval(getReadingsAndPublish, 5, (mqttClient, sparkClient), repeat=True)
+    OperationsScheduler.asyncRunAtInterval(getReadingsAndPublish, 1, (mqttClient, sparkClient), repeat=True)
     mqttClient.loop_forever()
-
 
 main()
